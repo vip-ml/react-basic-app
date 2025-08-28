1138dea8f84f61e2365328f389edde063c9903e3
Vipin George Abraham <georgevipin1@gmail.com>
2025-08-28 11:30:49 +0200

    Capture author/body and log post on submit

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,17 +1,40 @@
 import classes from './NewPost.module.css';
+import { useState } from 'react';
 
 
-function NewPost(props) {
+function NewPost({ onCancel }) {
+    const [enteredText, setEnteredText] = useState("");
+    const [enteredAuthor, setEnteredAuthor] = useState("");
 
+    function textHandler(event) {
+        setEnteredText(event.target.value);
+    }
+
+    function authorHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const postData = {
+            author: enteredAuthor,
+            body: enteredText
+        };
+        console.log(postData);
+    }
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={props.onTextChange} />
+                <textarea id="body" required rows={3} onChange={textHandler} />
             </p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required onChange={props.onAuthorChange} />
+                <input type="text" id="name" required onChange={authorHandler} />
+            </p>
+            <p className={classes.actions}>
+                <button type="button" onClick={onCancel}>Cancel</button>
+                <button>Submit</button>
             </p>
         </form>
     );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,27 +4,18 @@ import Post from "./Post";
 import classes from "./Posts.module.css"
 import { useState } from "react";
 
-function PostsList(props) {
+function PostsList({ isPosting, onStopPosting }) {
 
 
-    const [enteredText, setEnteredText] = useState("");
-    const [enteredAuthor, setEnteredAuthor] = useState("");
 
-    function textHandler(event) {
-        setEnteredText(event.target.value);
-    }
 
-    function authorHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
- 
     return (
         <>
-            {props.isPosting && (<Modal onClick={props.onStopPosting}>
-                <NewPost onTextChange={textHandler} onAuthorChange={authorHandler} />
+            {isPosting && (<Modal onClick={onStopPosting}>
+                <NewPost />
             </Modal>)}
             <ul className={classes.Posts} >
-                <Post author={enteredAuthor} content={enteredText} />
+
                 <Post author={"Kevin"} content={"I am 23 years old"} />
                 <Post author={"Hena"} content={"I am 20 years old"} />
             </ul >
